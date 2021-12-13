467db9583c66c4f91754add75cd18eec55cff3bf
Lemos <37798301+Lemos467@users.noreply.github.com>
2021-12-13 23:14:37 +0000

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+import CreateArea from "./CreateArea.jsx";
+import notes from "../notes.js";
+
+function App(){
+    const [notesArray,setNote] = useState([]);
+
+    function addNote(note){
+        setNote( prev => [...prev,note]);
+    }
+
+    function deleteNote(id){
+        
+        setNote(notesArray.filter(
+            (prev,index) => {
+                return index != id;
+            }
+        ));
+    }
+
+    return (
+        <div>
+            <Header />
+            <CreateArea 
+                onAdd={addNote}
+            />
+            {notesArray.map((note, idx) => (
+                <Note 
+                    key={idx}
+                    id={idx}
+                    title={note.title}
+                    content={note.content}
+                    addNote={addNote}
+                    onDelete={deleteNote}
+                />
+            ))}
+            
+            <Footer />
+        </div>
+        );
+};
+
+export default App;
